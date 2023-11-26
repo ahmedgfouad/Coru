35ddfb282e94401201fc1d7d7c4d97ba7bc59c7f
ahmedgfouad <ahmedgfouad2020@gmail.com>
2023-11-26 18:14:30 +0200

    refactor course details view

---
+++ lib/features/home/view/widgets/lesson_course_widget.dart
@@ -0,0 +1,28 @@
+import 'package:elearning_app/data/model/course_detials_model.dart';
+import 'package:elearning_app/features/home/view/widgets/lessons_tab_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LessonsCoursWidget extends StatelessWidget {
+  const LessonsCoursWidget({
+    super.key,
+    required this.course,
+  });
+
+  final CourseDetailsModel course;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          ' Lessons',
+          style: TextStyle(fontSize: 22.0.sp, fontWeight: FontWeight.bold),
+        ),
+        LessonsListWidget(lesson: course.lessons!),
+      ],
+    );
+  }
+}
+
